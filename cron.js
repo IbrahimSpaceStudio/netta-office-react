00ebee320c6486e3d5ea75be7cf1ccaf8441e8a0
100percentibrahim <ibrahimcpersonal@gmail.com>
2024-11-25 10:06:07 +0700

    refactor: fix all

---
+++ cron.js
@@ -0,0 +1,16 @@
+const axios = require("axios");
+
+const apiURL = process.env.REACT_APP_API_URL;
+
+async function findFailedJob() {
+  const formData = new FormData();
+  try {
+    const url = `${apiURL}/authapi/findjobfail`;
+    await axios.post(url, formData, { headers: { "Content-Type": "multipart/form-data" } });
+  } catch (error) {
+    console.error("Error fetching post slugs:", error);
+    process.exit(1);
+  }
+}
+
+findFailedJob();
